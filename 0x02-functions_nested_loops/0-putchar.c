13bdf3501a5c5bda4bb89791dd490481887e276c
AmponsahJ99 <amjust99@gmailcom>
2023-03-21 15:54:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main-main function
- * _putchar: print the alphabets putchar
- * Return: always 0
+ *main -  main function
+ *__putchar: print alphabets
+ *Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,6 +16,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 }
-
