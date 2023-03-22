ef73fdbc38177755667bceb89bf713eed1e2b921
AmponsahJ99 <amjust99@gmailcom>
2023-03-22 14:28:43 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,23 +3,31 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
+ *
  * Return: 1, 0 and -1
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
 	}
 }
