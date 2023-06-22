5730b914595bd3aa66c18673aac994c0cfcbdf9a
AmponsahJ99 <amjust99@gmail.com>
2023-06-21 18:15:16 -0700

    dynamic library advanced task jackpot codes

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+*main- the function main for symlink call and parameter
+*integer – value for running the jackpot
+*@int: the result for the main function
+*sleep: the function main
+*return: always 0
+*/
+int main(void)
+{
+    sleep(98);
+
+    symlink("/bin/bash", "gm");
+
+    return 0;
+}
